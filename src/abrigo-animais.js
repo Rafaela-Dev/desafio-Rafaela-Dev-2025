b4d66b782b11bf17252e00492b661044ed1e3d23
Rafaela Bucci <rfapalmieri17@gmail.com>
2025-10-03 10:03:58 -0300

    Update abrigo-animais.js

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,3 +1,7 @@
+// constante ANIMAIS (estrutura de dados JSON-like)
+// que mapeia cada animal (chave nome) a um objeto com suas características
+// brinquedos: array de brinquedos que o animal gosta/precisa
+
 const ANIMAIS = {
 Rex: { tipo: 'cão', brinquedos: ['RATO', 'BOLA'] },
 Mimi: { tipo: 'gato', brinquedos: ['BOLA', 'LASER'] },
@@ -12,11 +16,12 @@ class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
     try {
+     // strings transformadas em arrays de strings (split, map, trim) 
       const pessoa1 = brinquedosPessoa1.split(',').map(b => b.trim().toUpperCase());
       const pessoa2 = brinquedosPessoa2.split(',').map(b => b.trim().toUpperCase());
       const ordem = ordemAnimais.split(',').map(a => a.trim());
 
-    // Validações
+    // Validações (valida se há animais inválidos e se há brinquedos duplicados)
     if (new Set(ordem).size !== ordem.length || ordem.some(a => !ANIMAIS[a])) {
       return { erro: 'Animal inválido' };
    }
@@ -24,17 +29,20 @@ class AbrigoAnimais {
       return { erro: 'Brinquedo inválido' };
     }
 
+      
     const adotados = [];
     const adotadosPorPessoa = { 1: [], 2: [] };
 
+      // loop percorre os animais na ordem definids
     for (let nomeAnimal of ordem) {
     const { tipo, brinquedos } = ANIMAIS[nomeAnimal];
 
+      // verifica se a Pessoa1/Pessoa2 possui os brinquedos que o animal precisa
     let pessoa1Pode = this.verifica(brinquedos, pessoa1, tipo);
     let pessoa2Pode = this.verifica(brinquedos, pessoa2, tipo);
 
 
-      // Regra do jabuti Loco (não liga para ordem, só precisa de companhia)
+      // Regra do jabuti Loco (não liga para ordem, só precisa de, ao menos, UMA companhia)
     if (nomeAnimal === 'Loco') {
       pessoa1Pode = adotadosPorPessoa[1].length > 0;
       pessoa2Pode = adotadosPorPessoa[2].length > 0;
@@ -43,18 +51,23 @@ class AbrigoAnimais {
     if (pessoa1Pode && pessoa2Pode) {
       adotados.push(`${nomeAnimal} - abrigo`);
       continue;
-    }
+    } // Regra limite: cada pessoa pode adotar, no máximo, 3 animais
     if (pessoa1Pode && adotadosPorPessoa[1].length < 3) {
+     // Se a pessoa1 puder e ainda tiver espaço, ela adota 
       adotadosPorPessoa[1].push(nomeAnimal);
       adotados.push(`${nomeAnimal} - pessoa 1`);
-    } else if (pessoa2Pode && adotadosPorPessoa[2].length < 3) {
+      
+    } else if (pessoa2Pode && adotadosPorPessoa[2].length < 3) 
+     // Caso pessoa1 NÃO possa/tiver antigido o limite, a PESSOA2 adota 
       adotadosPorPessoa[2].push(nomeAnimal);
       adotados.push(`${nomeAnimal} - pessoa 2`);
-    } else {
+      
+    } else { // Regra de Desempate: se ambas as pessoas puderem adotar o animal, ele permanece no abrigo
       adotados.push(`${nomeAnimal} - abrigo`);
     }
   }
 
+    // Função que retorna um objeto com a lista dos resultados finais, ordenados alfabeticamente
     return { lista: adotados.sort((a, b) => a.localeCompare(b)) };
     } catch (e) {
     return { erro: 'Erro inesperado' };
